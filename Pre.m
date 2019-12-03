dee40a489a18aecbffbdc52bd2dd69685b99db2b
Soroush Omidvar <soroush.mid@gmail.com>
2019-12-03 18:03:19 +0330

    remove not applicable, refused, don't know data (<0 instead of -2)

--- Pre.m
+++ Pre.m
@@ -4,9 +4,10 @@ feat=[justNumbercsv(:,1:394) justNumbercsv(:,397:418)]; % remove labels from dat
 after_pre=feat;
 for i=1:size(feat,1)
     for j=1:size(feat,2)
-    if isequal(table2array(feat(i,j)),-2)
+        %if isequal(table2array(feat(i,j)),-2) % remove not applicable data
+        if table2array(feat(i,j))<0 % remove not applicable, refused, don't know data
             after_pre(i,j)={NaN};
-    end
+        end
     end
 end
 feat_with_NaN= after_pre;
